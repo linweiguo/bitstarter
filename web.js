f3c9bd07010c047729369d745882e07f79a20e54
Lin Weiguo <weiguo.lin@gmail.com>
2013-07-12 05:16:55 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
+var fs=require('fs');
+var infile="index.html";
+var outbuffer=new Buffer(40);
+
+outbuffer = fs.readFileSync(infile);
+var out = outbuffer.toString();
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!2');
+  response.send('Hello World!3');
 });
 
 var port = process.env.PORT || 5000;
